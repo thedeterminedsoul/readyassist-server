3750f11e32d61b119ba76abb583414fa73904a54
Pawan Tejwani <>
2020-05-12 09:20:24 +0530

    first api end to end

--- app/server.js
+++ app/server.js
@@ -2,12 +2,38 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// database settings
+var mysql = require('mysql')
+var conInfo = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "readyassist"
+}
+var connection = mysql.createConnection(conInfo);
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
 
 app.get('/students', (req, res) => {
    res.send('<h1>Hello students!</h1>');
+});
+
+app.get('/candidates', (req, res) => {
+    // in response I want to reply with all the candidate information
+    let sql = 'select * from candidates;';
+    connection.query(sql, function(err, data, fields){
+        if(err) {
+            res.status(500);
+            res.send(err);
+        }
+        let responseObject = {
+            data: data
+        }
+        res.send(responseObject);
+    })
 });
 
 
